ccc8a5ad335f4c779e0d92d7cd87481f26672fd6
Xenia Klimova <xenia.klimova@gmail.com>
2022-08-10 16:58:02 +0400

    initial

---
+++ js/scrolling.js
@@ -0,0 +1,90 @@
+
+
+/*var lastKnownScrollY = 0
+var currentScrollY = 0
+var ticking = false
+var idOfHeader = "header"
+var eleHeader = null
+const classes = {
+  pinned: "header-pin",
+  unpinned: "header-unpin",
+}
+function pin() {
+  if (eleHeader.classList.contains(classes.unpinned)) {
+    eleHeader.classList.remove(classes.unpinned)
+    eleHeader.classList.add(classes.pinned)
+  }
+}
+function unpin() {
+  if (
+    eleHeader.classList.contains(classes.pinned) ||
+    !eleHeader.classList.contains(classes.unpinned)
+  ) {
+    eleHeader.classList.remove(classes.pinned)
+    eleHeader.classList.add(classes.unpinned)
+  }
+}
+function onScroll() {
+  currentScrollY = window.pageYOffset
+  if (currentScrollY < lastKnownScrollY) {
+    pin()
+  } else if (currentScrollY > lastKnownScrollY) {
+    unpin()
+  }
+  lastKnownScrollY = currentScrollY
+}
+function myLoad() {
+  eleHeader = document.getElementById(idOfHeader)
+  document.addEventListener("scroll", onScroll, false)
+}
+
+window.onload = myLoad*/
+
+var lastKnownScrollY = 0
+var currentScrollY = 0
+var ticking = false
+var idOfHeader = "header"
+var eleHeader = null
+const classes = {
+  pinned: "header-pin",
+  unpinned: "header-unpin",
+}
+function onScroll() {
+  currentScrollY = window.pageYOffset
+  requestTick()
+}
+function requestTick() {
+  if (!ticking) {
+    requestAnimationFrame(update)
+  }
+  ticking = true
+}
+function update() {
+  if (currentScrollY < lastKnownScrollY) {
+    pin()
+  } else if (currentScrollY > lastKnownScrollY) {
+    unpin()
+  }
+  lastKnownScrollY = currentScrollY
+  ticking = false
+}
+function pin() {
+  if (eleHeader.classList.contains(classes.unpinned)) {
+    eleHeader.classList.remove(classes.unpinned)
+    eleHeader.classList.add(classes.pinned)
+  }
+}
+function unpin() {
+  if (
+    eleHeader.classList.contains(classes.pinned) ||
+    !eleHeader.classList.contains(classes.unpinned)
+  ) {
+    eleHeader.classList.remove(classes.pinned)
+    eleHeader.classList.add(classes.unpinned)
+  }
+}
+window.onload = function() {
+  eleHeader = document.getElementById(idOfHeader)
+  document.addEventListener("scroll", onScroll, false)
+}
+
